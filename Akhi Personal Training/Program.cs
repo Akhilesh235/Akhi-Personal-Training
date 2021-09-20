38578d8de134ad054951bc2e4e7f67055a07035f
Akhilesh235 <90238925+Akhilesh235@users.noreply.github.com>
2021-09-20 10:10:47 +0800

    Add project files.

---
+++ Akhi Personal Training/Program.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Akhi_Personal_Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int num1, num2;
+
+            Console.WriteLine("\nInput the first number : ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nInput the second number : ");
+            num2 = int.Parse(Console.ReadLine());
+
+            //1 Add
+            int sum = num1 + num2;
+            Console.WriteLine("Sum of both numbers is: " + sum);
+
+            //2 subtract
+            int sub = num1 - num2;
+            Console.WriteLine("Subratction of both numbers is: " + sub);
+
+            //multiply
+            int multiply = num1 * num2;
+            Console.WriteLine("Multiplication of both numbers is: " + multiply);
+
+            //Division
+            int divide = num1 / num2;
+            int remainder = num1 % num2;
+            Console.WriteLine("Division of both numbers is: " + divide + "\nWith a remainder of: " + remainder);
+
+
+
+
+
+
+
+            //int number1, number2, number3;
+
+            //Console.WriteLine("\n input first number");
+            //number1 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("\n input second number");
+            //number2 = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("\n input third number");
+            //number3 = int.Parse(Console.ReadLine());
+
+
+            //int result = number1 * number2 * number3;
+            //Console.WriteLine("the result is : " + result);
+
+
+
+            //int i, j, temp;
+
+            //Console.WriteLine("\nInput first number");
+            //i = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("\nInput second number");
+            //j = int.Parse(Console.ReadLine());
+
+            //temp = i;
+            //i = j;
+            //j = temp;
+            //Console.WriteLine("\nAfter Swapping");
+            //Console.WriteLine("\n first number: " + i);
+            //Console.WriteLine("\n second number: " + j);
+
+
+            //int i = 10, j = 20;
+
+            //if (i>j)
+            //{
+            //    Console.WriteLine("i greater than j");
+            //}
+
+            //if (i < j)
+            //{
+            //    Console.WriteLine("i smaller than j");
+            //}
+
+            //if (i == j)
+            //{
+            //    Console.WriteLine("i equal to j");
+            //}
+            //Coordinate point = new Coordinate(10, 20);
+
+            ////point.x = 10;
+            ////point.y = 20;
+            ////Coordinate point = new Coordinate();
+
+            //Console.WriteLine("x: " + point.x);
+            //Console.WriteLine("y: " + point.y);
+
+
+
+            //string firstName = "James";
+            //string lastName = "Bond";
+            //string code = "007";
+
+            //string fullName = $"{firstName} {lastName}, Code: {code}";
+
+            //Console.WriteLine(fullName);
+
+            //string str = @"this is a 
+            //                multi line 
+            //                string";
+            //Console.WriteLine(str);
+
+
+            //string text = "this is a \"string\" in c#";
+            //Console.WriteLine(text);
+
+
+
+
+            //char[] chars = { 'H', 'E', 'L', 'L', 'O' };
+
+            //Console.WriteLine(chars);
+
+            //string str1 = new string(chars);
+            //String str2 = new String(chars);
+
+            //foreach (char c in str1)
+            //{
+            //    Console.WriteLine(c);
+            //}
+
+
+
+            //foreach (char c in str2)
+            //{
+            //    Console.WriteLine("\n" + c);
+            //}
+
+            //var i = 10;
+            //Console.WriteLine("Type of i is {0}", i.GetType());
+
+            //var str = "Akhilesh";
+            //Console.WriteLine("Type of str is {0}", str.GetType());
+
+            //var dbl = 10.2;
+            //Console.WriteLine("type of dbl is {0}", dbl.GetType());
+
+            //var isValid = true;
+            //Console.WriteLine("type of isValid is {0}", isValid.GetType());
+
+            //Array arr = new[] { 1, 2, 3, 4, 5 };
+            //Console.WriteLine("type of arr is {0}", arr.GetType());
+
+
+
+            //int i = 1;
+            //int j = ++i;
+            //int k = --j;
+
+
+            //Console.WriteLine(k);
+
+
+
+            //int i = 100,
+            //    j = 10,
+            //    k = 4;
+
+
+
+
+
+            //int i = 100, j = 10, k = 77;
+            //Console.WriteLine("the value of i : " + i);
+            //Console.WriteLine("the value of j : " + j);
+            //Console.WriteLine("the value of k : " + k);
+
+
+            //int num = 100;
+            //float rate = 10.2f;
+            //double amount = 100.5;
+            //char code = 'C';
+            //bool isValid = true;
+            //string name = "Steve";
+
+            //Console.WriteLine(num + "\n");
+            //Console.WriteLine(rate);
+            //Console.WriteLine(amount);
+            //Console.WriteLine(code);
+            //Console.WriteLine(isValid);
+            //Console.WriteLine(name);
+
+            //Console.ReadLine();
+
+            //string message = "Hello World!";
+
+            //Console.WriteLine(message);
+            Console.Read();
+        }
+    }
+}
